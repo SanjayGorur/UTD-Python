e6c1d2d75a8f52bfd6a0ffaab8fd79172ec909be
SanjayGorur <51182077+SanjayGorur@users.noreply.github.com>
2020-04-04 21:02:48 -0500

    Session1.py
    First session with new student.

---
+++ Session1.py
@@ -0,0 +1,41 @@
+#1st Session
+
+x = int(input("Enter a number: "))
+"Perimeter: val, Area: val"
+y = int(input("Enter another number: "))
+per1 = (2 *(x + y))
+area = (x * y)
+print("Perimeter: ", per1, "\nArea: ", area)
+print("Area: ", int(input("Enter a number: ")) * int(input("Enter another number: ")))
+
+
+var = 1.3
+if(var>=5):
+  print("above 5")
+elif(var>=2):
+  print("above 2")
+elif(var==0):
+  print("it is 0")
+elif(var >= 0.5):
+  print("Rational")
+else:
+  print("Else part")
+
+
+day = input("Enter a weekday: ")
+if(day == "sunday" or day == "monday" or day == "tuesday"):
+  print("the day is 1 of the first 3 days.")
+else:
+  print("Sorry, it is one of the last 4 days. :( ")
+  
+
+a = input("Enter a number: ")
+b= input("Enter another number: ")
+print("a: ", a)
+print("b: ", b)
+if(a > b):
+  print("a is bigger than b")
+elif(b > a):
+  print("b is bigger than a")
+else:
+  print("they are equal")
