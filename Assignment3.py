8af224d231b84c5198d869a2aae37e42f73feb5d
SanjayGorur <51182077+SanjayGorur@users.noreply.github.com>
2020-04-12 21:38:15 -0500

    Updated

--- Assignment3.py
+++ Assignment3.py
@@ -1,15 +1,4 @@
-# {"first":{"f1":1, "f2":2}, "second":{"s2":2, "s3":3 , "second1":{"ss1":1,"ss2":2}},"third":3}
-
-# "first"
-#   "f1":1
-#   "f2":2
-# "second"
-#   "s2":2
-#   "s3":3
-#   "second1"
-#     "ss1":1
-#     "ss2":2
-# "third": 3
+#Lesson Time!!
 
 d = {"first":{"f1":1, "f2":2}, "second":{"s2":2, "s3":3 , "second1":{"ss1":1,"ss2":2}},"third":3}
 
@@ -17,19 +6,16 @@ check = "   "
 count = 0
 
 def go(d):
-  count = 0
   for key in d:
     if(type(d.get(key)) == dict):
       print(key)
       for k in d.get(key):
         print(check * count, repeat(d.get(key), k))
-    else:
-      print(str(key) + ":" + str(d.get(key)))
+    elif(key != "third"):
+        print(str(key) + ":" + str(d.get(key)))
 
 def repeat(di, ke):
-    subs = count
     if(type(di.get(ke)) == dict):
-      subs += 1
       print(check * count, ke)
       return(loop(di.get(ke)))
     coord = str(ke) + ":" + str(di.get(ke))
@@ -38,7 +24,12 @@ def repeat(di, ke):
 def loop(dicti):
     for k in dicti:
       print((check) + str(k) + ":" + str(dicti[k]))
+    last(d, 2)
     return ""
+
+def last(dict, num):
+  print(str(list(dict)[num]) + ":" + str(d.get(list(dict)[num])))
+  return ""
 
 
 go(d)
