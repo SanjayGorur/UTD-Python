bf177af9510705b81f03429905ba8fb0b4da2479
SanjayGorur <51182077+SanjayGorur@users.noreply.github.com>
2020-05-02 21:18:14 -0500

    Intro to Machine Learning w/ Student
    Originally CSV file

---
+++ IntroML.py
@@ -0,0 +1,52 @@
+# Originally CSV file - imported to Google collab as .ipynb
+# commented
+# Google collab - like jupyter notebook
+# cloud based
+# GPU / TPU - Tensor Processing Unit
+# Machine Learning
+# 1. Supervised Learning
+# 	Linear Regression
+# 	Random forest
+# 	Decision trees
+# 	..
+# 2. Unsupervised learning
+# 	Clustering
+# 3. Reinforcement learning
+# 	Robotics
+
+# number of bedrooms, kitchen, balcony, area, location, ----> $1,000,000
+# X							Y
+# 1. 3, 1, 2, 2000, dallas - --> 50000$
+# 2.
+# .
+# .
+# .
+# 9999
+# 4, 3 lasfldasldf ----> 
+# y = mx + c
+# y = w0 + w1x1 + w2x2 + w3x3 ..... wnxn
+
+# Train (0.8), Test(0.2) 
+# 1000 images of a human face
+# 50 
+
+# 3
+
+# x1 x2 x3 x4
+# 1  2  1  2
+# 2  3  4  5 
+# 9 9 10   1
+
+# .......
+
+# ....
+
+# LSTM - Long short term memory
+
+# Feature Engineering - Lions, Cats
+
+
+# y = sin(x) -1<1
+# pandas -> it is good for manipulating csv files
+# data manipulation
+# dataframe
